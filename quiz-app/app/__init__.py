e634e22591c45d32b26daa90a884e2db9c4aeabd
elbatouri rabi <elbatouri@gmail.com>
2024-05-21 20:43:24 +0100

    update files

--- quiz-app/app/__init__.py
+++ quiz-app/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'main.login'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -13,8 +14,10 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     login_manager.init_app(app)
+    
+    migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
-    from app import routes
+    from app import routes, models
     app.register_blueprint(routes.bp)
 
     return app
