78858dac8745be79ee8e142fe2ea5ee4d9353ff8
elbatouri rabi <elbatouri@gmail.com>
2024-06-02 18:33:07 +0100

    add the top 10 scorrer and the comment section featers

--- quiz-app/app/__init__.py
+++ quiz-app/app/__init__.py
@@ -18,5 +18,10 @@ def create_app(config_class=Config):
 
     from app import routes, models
     app.register_blueprint(routes.bp)
-
+    
+    # Context processor to add enumerate to Jinja2 context
+    @app.context_processor
+    def utility_processor():
+        return dict(enumerate=enumerate)
+    
     return app
