7a6977f1dcfd8283b5af7c2c1c21c5feab8a8836
elbatouri rabi <elbatouri@gmail.com>
2024-06-05 21:09:01 +0100

    fix the nav bar distance

--- quiz-app/config.py
+++ quiz-app/config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://reda:reda123X@52.91.135.43/quiz_app'
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
